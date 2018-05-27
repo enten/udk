88b3e454915be5a4309d8ce718bc2d7a961c4ec0
Steven Enten <steven@enten.fr>
2018-05-27 16:01:11 +0200

    udk/angular: ng-udkc configuration support

--- angular/lib/devContainer.js
+++ angular/lib/devContainer.js
@@ -15,15 +15,43 @@ Runtime.extendRuntime(module, {
     }
 
     if (!this._builderContext[project]) {
+      const targetSpecifier = {
+        project,
+        target: 'udk'
+      };
+
+      const partialBuilderContext = { logger };
+
+      if (process.argv.length === 3 && process.argv[2][0] !== '-') {
+        targetSpecifier.configuration = process.argv[2];
+      } else if (process.argv.length > 2) {
+        let configuration;
+
+        for (let i = 2; i < process.argv.length; ++i) {
+          let arg = process.argv[i];
+
+          if (arg[0] === '-') {
+            if (arg === '-c' || arg === '-configuration' || arg === '--configuration') {
+              configuration = process.argv[i + 1];
+
+              break;
+            }
+
+            if (arg.startsWith('-c=') || arg.startsWith('-configuration=') || arg.startsWith('--configuration=')) {
+              configuration = arg.split('=')[1];
+
+              break;
+            }
+          }
+        }
+
+        if (configuration) {
+          targetSpecifier.configuration = configuration;
+        }
+      }
+
       this._builderContext[project] = architect$.pipe(
-        concatMap(architect => {
-          return prepareBuilder(architect, {
-            project,
-            target: 'udk'
-          }, {
-            logger
-          })
-        })
+        concatMap(architect => prepareBuilder(architect, targetSpecifier, partialBuilderContext))
       ).toPromise();
     }
 
